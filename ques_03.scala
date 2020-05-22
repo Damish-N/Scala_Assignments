37853926e31b9ccf8bc511931fcc3ae79d537174
Damish <damishnisal100@gmail.com>
2020-05-22 19:22:30 +0530

    01_Assignment_submission

--- ques_03.scala
+++ ques_03.scala
@@ -3,9 +3,9 @@ object ques_03 extends App {
         var cover_page_price : Double = (24.95)*(0.6)*books;
         var shipping_cost : Double  = 0.0;
         if(books > 50) {
-          shipping_cost= (50)+((books-50)*(0.75));
+          shipping_cost= (3)+((books-50)*(0.75));
         }else{
-          shipping_cost= (books * 3) ;
+          shipping_cost= 3 ;
         }
         var cost_of_books : Double = cover_page_price + shipping_cost ;
         return cost_of_books;
